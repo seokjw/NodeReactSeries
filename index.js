991744432067c996bd0aed6f4a845c3aefe6d016
seokjw <wldn9020@daum.net>
2024-01-12 23:18:56 +0900

    [feat] 비밀 설정 정보 관리

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -11,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ggowhj3436:PCnivhlHlFvV30KH@cluster0.0g9ndhw.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Conneted...'))
 .catch(err => console.log(err))
